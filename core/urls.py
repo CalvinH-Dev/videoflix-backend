4857f44a782b594f102c154b54cc3a8949daf93b
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-03-21 16:40:14 +0100

    fix: healthcheck now visible without authentication

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class HealthCheckView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request):
         return Response({"status": "ok"})
 

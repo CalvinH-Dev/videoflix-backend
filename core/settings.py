9bd7192ee508164eba0fce27889f035f9d939b46
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-03-23 21:19:37 +0100

    add: zed live server to allowed hosts in dev added.

--- core/settings.py
+++ core/settings.py
@@ -62,6 +62,8 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://127.0.0.1:5500",
         "http://localhost:5173",
+        "http://127.0.0.1:57391",
+        "http://localhost:57391",
     ]
     ALLOWED_HOSTS = []
     STATIC_URL = "static/"
